a84a2c74273626f93112e4c123db7041fb37ca14
George Zogopoulos-Papaliakos <tailwhipx4@gmail.com>
2021-05-19 21:53:27 +0200

    Fixed indentation

--- Ardupilog.m
+++ Ardupilog.m
@@ -629,63 +629,63 @@ classdef Ardupilog < dynamicprops & matlab.mixin.Copyable
         function newlog = filterMsgs(obj,msgFilter)
         % Filter message groups in existing Ardupilog
         
-        % Get the logMsgGroups names and ids
-        msgNames = obj.msgsContained;
-        msgIds = zeros(1,length(msgNames));
-        for i=1:length(msgNames)
-            msgName = msgNames{i};
-            msgIds(i) = obj.(msgName).typeNumID;
-        end
-        
-        % Check for validity of the input msgFilter
-        if ~isempty(msgFilter)
-            if iscellstr(msgFilter) %obj.msgFilter is a cell-array of strings
-                invalid = find(ismember(msgFilter,msgNames)==0);
-                for i=1:length(invalid)
-                    warning('Invalid element in provided message filter: %s',msgFilter{invalid(i)});
-                end
-            else %msgFilter is an array of msgId's
-                invalid = find(ismember(msgFilter,msgIds)==0);
-                for i=1:length(invalid)
-                    warning('Invalid element in provided message filter: %d',msgFilter(invalid(i)));
+            % Get the logMsgGroups names and ids
+            msgNames = obj.msgsContained;
+            msgIds = zeros(1,length(msgNames));
+            for i=1:length(msgNames)
+                msgName = msgNames{i};
+                msgIds(i) = obj.(msgName).typeNumID;
+            end
+
+            % Check for validity of the input msgFilter
+            if ~isempty(msgFilter)
+                if iscellstr(msgFilter) %obj.msgFilter is a cell-array of strings
+                    invalid = find(ismember(msgFilter,msgNames)==0);
+                    for i=1:length(invalid)
+                        warning('Invalid element in provided message filter: %s',msgFilter{invalid(i)});
+                    end
+                else %msgFilter is an array of msgId's
+                    invalid = find(ismember(msgFilter,msgIds)==0);
+                    for i=1:length(invalid)
+                        warning('Invalid element in provided message filter: %d',msgFilter(invalid(i)));
+                    end
                 end
             end
-        end
-        
-        newlog = copy(obj); % Create the new log object
-        newlog.msgFilter = msgFilter; % Store the filter that yielded this log
-        deletedMsgNames = repmat({''}, 1, 1);
-        % Set the LineNos of any messages due for deletion to empty
-        for i = 1:length(msgNames)
-            msgName = msgNames{i};
-            msgId = newlog.(msgName).typeNumID;
-            if iscellstr(newlog.msgFilter)
-                if ~ismember(msgName,newlog.msgFilter)
-                    newlog.(msgName).LineNo = []; % Mark the message group for deletion
-                    deletedMsgNames{1, end+1} = msgName;
+
+            newlog = copy(obj); % Create the new log object
+            newlog.msgFilter = msgFilter; % Store the filter that yielded this log
+            deletedMsgNames = repmat({''}, 1, 1);
+            % Set the LineNos of any messages due for deletion to empty
+            for i = 1:length(msgNames)
+                msgName = msgNames{i};
+                msgId = newlog.(msgName).typeNumID;
+                if iscellstr(newlog.msgFilter)
+                    if ~ismember(msgName,newlog.msgFilter)
+                        newlog.(msgName).LineNo = []; % Mark the message group for deletion
+                        deletedMsgNames{1, end+1} = msgName;
+                    end
+                elseif isnumeric(newlog.msgFilter)
+                    if ~ismember(msgId,newlog.msgFilter)
+                        newlog.(msgName).LineNo = []; % Mark the message group for deletion
+                        deletedMsgNames{1, end+1} = msgName;
+                    end
+                else
+                    error('msgFilter type should have passed validation by now and I shouldnt be here');
                 end
-            elseif isnumeric(newlog.msgFilter)
-                if ~ismember(msgId,newlog.msgFilter)
-                    newlog.(msgName).LineNo = []; % Mark the message group for deletion
-                    deletedMsgNames{1, end+1} = msgName;
-                end
-            else
-                error('msgFilter type should have passed validation by now and I shouldnt be here');
             end
-        end
-        
-        % Update the msgsContained attribute
-        newlog.msgsContained = setdiff(obj.msgsContained, deletedMsgNames);
-        
-        newlog = newlog.deleteEmptyMsgs();  
-        
-        % Update the number of actual included messages
-        newlog.numMsgs = 0;
-        newMsgNames = newlog.msgsContained;
-        for i = 1:length(newMsgNames)
-            msgName = newMsgNames{i};
-            newlog.numMsgs = newlog.numMsgs + length(newlog.(msgName).LineNo);
-        end
+
+            % Update the msgsContained attribute
+            newlog.msgsContained = setdiff(obj.msgsContained, deletedMsgNames);
+
+            newlog = newlog.deleteEmptyMsgs();  
+
+            % Update the number of actual included messages
+            newlog.numMsgs = 0;
+            newMsgNames = newlog.msgsContained;
+            for i = 1:length(newMsgNames)
+                msgName = newMsgNames{i};
+                newlog.numMsgs = newlog.numMsgs + length(newlog.(msgName).LineNo);
+            end
         
         end
         
